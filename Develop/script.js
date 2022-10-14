78d76800eafb03122ffa3b9d9f3e523c29c8ab1c
Karley <karley@Karleys-MacBook-Pro.local>
2022-10-13 21:21:10 -0400

    Cleaned up youtube javascript

--- Develop/script.js
+++ Develop/script.js
@@ -44,52 +44,11 @@ function fetchYoutubeData() {
     })
     .then (function (data) {
         console.log(data);
-        // videoId = data.items[0].id.videoId;
         videoId = '7Gc4PVl_QeQ'
-        // var tag = document.createElement('script');
-        // tag.src = "https://www.youtube.com/iframe_api";
-        // var firstScriptTag = document.getElementsByTagName('script')[0];
-        // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
         player.loadVideoById(videoId, 0);
-        // onYouTubeIframeAPIReady();
     })
 }
-
-
-
-// player.playVideo();
-
-// Play video automatically when loaded
-
-// function onPlayerStateChange(event) {
-//     if (event.data == YT.PlayerState.CUED) {
-//         event.target.playVideo();
-//         console.log('ready');
-//         }
-//     }
-
-// 4. The API will call this function when the video player is ready.
-// function onPlayerReady(event) {
-// event.target.playVideo();
-// }
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-
-// var done = false;
-// function onPlayerStateChange(event) {
-// if (event.data == YT.PlayerState.PLAYING && !done) {
-//     setTimeout(stopVideo, 6000);
-//     done = true;
-//     }
-// }
-
-// function stopVideo() {
-// player.stopVideo();
-// }        
-
-
+       
 
 // **COCKTAIL FEATURE**
 
